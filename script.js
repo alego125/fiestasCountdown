4b42a007dfa2475c8f9befb6527e9ea1f650b67b
Alejandro Gomez <76167482+alego125@users.noreply.github.com>
2021-12-24 18:27:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const d = document;
+
+d.addEventListener('DOMContentLoaded', e => {
+    const $countdown = d.getElementById("countdown"),
+        countdownDate = new Date("Dec 25 2021 00:00:00").getTime(),
+        $alarm = d.createElement("audio"),
+        $btnAlarma = d.getElementById("btn-alarm");
+
+    let countDownTempo = setInterval(() => {
+        let now = new Date().getTime(),
+            limitTime = countdownDate - now,
+            days = Math.floor(limitTime / (1000 * 60 * 60 * 24)),
+            hours = ("0" + Math.floor((limitTime % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)))).slice(-2),
+            minutes = ("0" + Math.floor((limitTime % (1000 * 60 * 60) / (1000 * 60)))).slice(-2),
+            seconds = ("0" + Math.floor((limitTime % (1000 * 60) / 1000))).slice(-2);
+
+        $countdown.innerHTML = `<h3 style="font-size: 100px;">${days} dias ${hours} horas ${minutes} minutos y ${seconds} segundos</h3>`;
+
+        if (limitTime < 0) {
+            clearInterval(countDownTempo);
+            $countdown.innerHTML = `<h3 style="font-size: 100px;">${"🎅🎅FELIZ NAVIDAD🎅🎅"}</h3>`;
+            $btnAlarma.classList.add("visible");
+            $alarm.src = "assets/alarma.mp3";
+            $alarm.play();
+            $btnAlarma.addEventListener('click', () => {
+                $btnAlarma.classList.remove("visible");
+                $alarm.pause();
+                $alarm.currentTime = 0;
+            });
+        }
+    }, 1000);
+
+});
